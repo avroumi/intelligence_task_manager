b7120eaaff6f39440344cb316e3fbc7327c88719
Avroumi <avroumi97@gmail.com>
2026-06-18 16:14:08 +0300

    add clean main.py

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from database.mission_db import MissionDB
 
 from routes.agent_routes import router_agent
 from routes.mission_routes import router_mission
+from routes.report_routes import router_report
 
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from contextlib import asynccontextmanager
+from logs.logger_param import logger
 
 
 
 
 
 db = DataBaseConnection()
-agent_db = AgentDB(db)
-mission_db = MissionDB(db, agent_db)
+
 
 
 @asynccontextmanager
@@ -24,43 +25,19 @@ async def lifespan(app : FastAPI):
 
 app = FastAPI(title="Intelligence Task Manager", lifespan=lifespan)
 
+@app.middleware("http")
+async def middleware(req : Request, call_next):
+    logger.info(f"START | {req.method} |{req.url.path} ")
+    response = await call_next(req)
+    logger.info(f"END | {req.method} | {req.url.path} | status={response.status_code} ")
+
+    return response
+
+
+
 
 app.include_router(router_agent)
 app.include_router(router_mission)
+app.include_router(router_report)
 
 
-
-# print(repo.create_agent({"name":"avroumi", "specialty": "boomer", "completed_missions": "1","failed_missions": "1", "agent_rank" : "Senior"}))
-# print(repo.get_all_agents())
-# # print(repo.get_agent_by_id(3))
-# print(repo.deactivate_agent(1))
-# print(repo.get_agent_by_id(1))
-# print(repo.increment_failed(1))
-# print(repo.get_agent_by_id(1))
-# print(repo.increment_failed(1))
-# # print(repo.get_agent_by_id(1))
-# print(repo.get_agent_performance(5))
-# print(repo.count_active_agents())
-
-
-# print(repo_mission.create_mission_({"title" : "lebvanon","description": "balbala", 
-#                                     "location" : "yes" , "difficulty" : "4" , "importance" : "9"}))
-
-# risk = {"title" : "lebvanon","description": "balbala", 
-#                                     "location" : "yes" , "difficulty" : "4" , "importance" : "7"}
-
-# print(int(risk["importance"]))
-# print(repo_mission.assign_mission(10, 3))
-# print(repo_mission.get_all_missions())
-# print(repo_mission.get_open_missions_by_agent(1))
-# print(repo_mission.get_all_missions())
-
-# print(repo_mission.count_by_status("J"))
-# print(repo_mission.get_top_agent())
-# # print(repo_mission.update_mission_status(1,"IN_PROGRESS"))
-# print(repo_mission.get_missions_by_id(1))
-# print(repo_mission.count_open_missions())
-# print(repo_mission.get_open_missions_by_agent(2))
-# print(repo_mission.assign_mission(2,2))
-# print(repo.get_agent_by_id(2))
-# print(repo_mission.get_missions_by_id(2))
